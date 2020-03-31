cdf552f2bf6b76e44393f78e6adbbfc9a2b5aac7
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-03-31 12:40:47 +0200

    home controller added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+        public IActionResult Authenticate()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
