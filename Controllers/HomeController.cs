572f140ef57836fadb184489c8145968f5caa673
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-03-31 13:04:57 +0200

    Added cookie, creates when trying to access authorized page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication;
+
 
 namespace Auth.Controllers
 {
@@ -14,8 +18,28 @@ namespace Auth.Controllers
         {
             return View();
         }
+
         public IActionResult Authenticate()
         {
+            var Claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"Bob"),
+                new Claim(ClaimTypes.Email,"Bob@gmail.com"),
+                new Claim("Vouch.Says", "Trusted Source"),
+            };
+            var LicenseClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"Jen Jenkinson"),
+                new Claim("DrivingLicense", "Dodgy but cool"),
+            };
+
+            var IdentityClaim = new ClaimsIdentity(Claims,"Vouched Identity");
+            var IdentityLicenseClaims = new ClaimsIdentity(LicenseClaims, "Government");
+
+            var userPrincipal = new ClaimsPrincipal(new[] {IdentityClaim, IdentityLicenseClaims});
+
+            HttpContext.SignInAsync(userPrincipal);
+
             return RedirectToAction("Index");
         }
     }
